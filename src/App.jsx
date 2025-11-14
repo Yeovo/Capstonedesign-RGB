2f8b67d7dd414f02f6ed6ecebe8156cfe9c3dfa6
Yeovo <hjmgra97@gmail.com>
2025-11-14 19:33:26 +0900

    Refactor App component and update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Upload from './pages/Upload.jsx'
-import Result from './pages/Result.jsx'
-import Test from './pages/Test.jsx'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Upload from "./pages/Upload.jsx";
+import Test from "./pages/Test.jsx";
+import Result from "./pages/Result.jsx";
+import SavedPalettes from "./pages/SavedPalettes.jsx";
 
 export default function App() {
   return (
-    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
-      <nav style={{ marginBottom: 20 }}>
-        <Link to="/" style={{ marginRight: 10 }}>Home</Link>
-        <Link to="/upload" style={{ marginRight: 10 }}>Upload</Link>
-        <Link to="/test">Test</Link>
+    <div style={{ maxWidth: 1240, margin: "0 auto", padding: "20px" }}>
+      {/* 상단 네비 */}
+      <nav
+        style={{
+          display: "flex",
+          gap: 24,
+          justifyContent: "center",
+          marginBottom: 18,
+          flexWrap: "wrap",
+        }}
+      >
+        <Link to="/test" style={navStyle}>Test</Link>
+        <Link to="/upload" style={navStyle}>Upload</Link>
+        <Link to="/palettes" style={navStyle}>Palettes</Link>
+        <Link to="/" style={navStyle}>Home</Link>
       </nav>
 
+      {/* 라우팅 */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/upload" element={<Upload />} />
-        <Route path="/result" element={<Result />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/palettes" element={<SavedPalettes />} />
       </Routes>
     </div>
-  )
+  );
 }
+
+const navStyle = {
+  color: "#2d7ef7",
+  fontSize: 20,
+  fontWeight: 700,
+  textDecoration: "none",
+};
+
